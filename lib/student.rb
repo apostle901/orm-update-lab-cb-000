956f78336148ede1a9e5ef34b71926a90c39f5f0
Jawann <jawann@gmail.com>
2018-02-19 04:27:50 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,3 +8,55 @@ class Student
   def initialize(id = nil, name, grade)
     @id, @name, @grade = id, name, grade
   end
+
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade TEXT
+    )
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = "DROP TABLE IF EXISTS students"
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = <<-SQL
+      INSERT INTO students (name, grade)
+      VALUES (?, ?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.grade)
+  end
+
+  def self.create(name: , grade: )
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
+
+  def self.new_from_db(row)
+    stud = Student.new
+    stud.id, stud.name, stud.grade = row[0], row[1], row[2]
+    stud
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+end
